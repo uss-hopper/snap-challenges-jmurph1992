d2ef99a1fa112510d853f39ef3088fb7df87e456
Justin Murphy <j.murphyjt@gmail.com>
2019-07-09 08:42:44 -0600

    Finished the challenge

---
+++ snap7.9/functionchallenge.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2){
+	return num1 + num2;
+}
+
+let num3 = addNumbers(123,456);
+
+console.log (num3);
+
+function addWords(word1, word2) {
+	return word1 + word2;
+}
+
+let wordCombo = addWords("Hello,", " this is my word combo!");
+
+console.log (wordCombo);
