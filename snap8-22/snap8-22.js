1827fdfbe30d564985d76a3f7c4037fae7e812b0
jmurph1992 <j.murphyjt@gmail.com>
2019-08-22 08:36:06 -0600

    first commit with my answers

---
+++ snap8-22/snap8-22.js
@@ -0,0 +1,16 @@
+let array1 = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
+
+let reduced = array1.reduce(function(accumulator, currentValue) {
+	return accumulator + currentValue;
+});
+
+console.log(reduced);
+
+
+array2 = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+
+let string = array2.reduce(function(accumulator, currentValue, currentIndex) {
+	return currentIndex === 0 ? currentValue : accumulator + ' ' + currentValue;
+}, '');
+
+console.log(string);
