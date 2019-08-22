39ad36b4406ae2cc45c2c9f0776b03989215171e
jmurph1992 <j.murphyjt@gmail.com>
2019-08-22 09:00:07 -0600

    removed more unnecessay items

--- snap8-22/snap8-22.js
+++ snap8-22/snap8-22.js
@@ -9,8 +9,8 @@ console.log(reduced);
 
 array2 = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
 
-let string = array2.reduce(function(accumulator, currentValue, currentIndex) {
-	return currentIndex === 0 ? currentValue : accumulator + ' ' + currentValue
+let string = array2.reduce(function(accumulator, currentValue) {
+	return accumulator + ' ' + currentValue
 });
 
 console.log(string);
